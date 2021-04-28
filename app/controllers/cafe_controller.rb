464077dca8766eae5bcd4932fe575f25534d9509
Yuusuke Nagata <yusukenagata@hotmail.com>
2021-04-28 10:44:18 +1000

    up to date

--- app/controllers/cafe_controller.rb
+++ app/controllers/cafe_controller.rb
@@ -2,19 +2,19 @@ class CafeController < ApplicationController
   # IMPORTANT!! REMOVE FOR PRODUCTION!
   skip_before_action :verify_authenticity_token, only: [:create]
 
-  @@menu = {
-    latte: 4.00,
-    scone: 5.00,
-    tea: 3.00,
-  }
+  # @@menu = {
+  #   latte: 4.00,
+  #   scone: 5.00,
+  #   tea: 3.00,
+  # }
 
   def index
     @currency_symbol = "$"
-    @menu = @@menu
+    @menu = MenuItem.order(:name)
   end
 
   def about
-    render json: @@menu
+    render json: MenuItem.all
   end
 
   def create
@@ -23,6 +23,7 @@ class CafeController < ApplicationController
   end
 
   def show
-    @item = { name: params[:item_name], price: @@menu[params[:item_name].to_sym] }
+    # @item = { name: params[:item_name], price: @@menu[params[:item_name].to_sym] }
+    @item = MenuItem.find_by_name(params[:item_name])
   end
 end
